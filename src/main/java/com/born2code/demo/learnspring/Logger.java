b1cf5dfddd9eef45c75672112c53288bf73d978b
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-07-15 19:52:16 +0600

    Completed 04-01_The_Autowired_Annotation

--- src/main/java/com/born2code/demo/learnspring/Logger.java
+++ src/main/java/com/born2code/demo/learnspring/Logger.java
@@ -1,24 +1,35 @@
 package com.born2code.demo.learnspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
+	// @Autowired
 	private ConsoleWriter consoleWriter;
+	// @Autowired
 	private FileWriter fileWriter;
+
+	@Autowired
+	public Logger(ConsoleWriter consoleWriter) {
+		this.consoleWriter = consoleWriter;
+	}
 
 	public LogWriter getConsoleWriter() {
 		return consoleWriter;
 	}
 
-	public void setConsoleWriter(ConsoleWriter consoleWriter) {
-		this.consoleWriter = consoleWriter;
-	}
+	// @Autowired
+	 public void setConsoleWriter(ConsoleWriter consoleWriter) {
+	 this.consoleWriter = consoleWriter;
+	 }
 
 	public LogWriter getFileWriter() {
 		return fileWriter;
 	}
 
-	public void setFileWriter(FileWriter fileWriter) {
-		this.fileWriter = fileWriter;
-	}
+	 @Autowired
+	 public void setFileWriter(FileWriter fileWriter) {
+	 this.fileWriter = fileWriter;
+	 }
 
 	public void writeConsole(String text) {
 		consoleWriter.write(text);
