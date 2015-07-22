eb36f038ff44878ffb71fe277cc7d3822d4e6468
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-07-22 15:54:46 +0600

    Completed 04-08 Setting Property Values through Annotations

--- src/main/java/com/born2code/demo/learnspring/App.java
+++ src/main/java/com/born2code/demo/learnspring/App.java
@@ -12,9 +12,8 @@ public class App
     public static void main( String[] args )
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("com/born2code/demo/learnspring/beans/beans.xml");
-    	Logger logger = (Logger) context.getBean("logger");
-    	logger.writeConsole("Inside Console");
-    	logger.writeFile("Inside File");
+    	Cat cat = (Cat) context.getBean("cat");
+    	cat.speak();
     	((ClassPathXmlApplicationContext) context).close();
     }
 }
--- src/main/java/com/born2code/demo/learnspring/LogWriter.java
+++
@@ -1,5 +0,0 @@
-package com.born2code.demo.learnspring;
-
-public interface LogWriter {
-	public void write(String text);
-}
