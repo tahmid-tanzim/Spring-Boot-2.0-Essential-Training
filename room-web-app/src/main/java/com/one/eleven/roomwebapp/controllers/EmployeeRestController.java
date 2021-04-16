30c02e59d40a33816276a6a5678f4ca8a367ddcc
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-04-16 14:33:07 +0600

    RestController Annotation

---
+++ room-web-app/src/main/java/com/one/eleven/roomwebapp/controllers/EmployeeRestController.java
@@ -0,0 +1,24 @@
+package com.one.eleven.roomwebapp.controllers;
+
+import com.one.eleven.roomwebapp.models.Employee;
+import com.one.eleven.roomwebapp.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeRestController {
+    private final EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
