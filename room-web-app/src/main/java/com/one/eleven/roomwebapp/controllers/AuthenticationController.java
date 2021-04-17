92f568dc0472278d9c298a7d9d637e58cd8f6055
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-04-17 15:21:07 +0600

    Spring Security

---
+++ room-web-app/src/main/java/com/one/eleven/roomwebapp/controllers/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.one.eleven.roomwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+}
